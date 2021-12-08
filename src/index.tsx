b7cc8794ad3db3d8ec74255987029dc10326e1df
Joakim Gyllenskepp <jockegy@gmail.com>
2021-12-08 22:50:53 +0100

    Upgrade to react 18 rc

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import reportWebVitals from "./reportWebVitals";
 const { worker } = require("./mocks/browser");
 worker.start();
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
